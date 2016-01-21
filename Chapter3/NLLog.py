0ac12ab7916334f29c1e69c947d3f1eeba6d4517
Michael Estes <mjestes1994@gmail.com>
2016-01-21 10:11:02 -0500

    Added comments, SVM

--- Chapter3/NLLog.py
+++ Chapter3/NLLog.py
@@ -8,15 +8,10 @@ from matplotlib.colors import ListedColormap
 import numpy as np
 import matplotlib.pyplot as plt
 
-# This program is a simulation of a data set with a non-linear separation region.
-# The data is gathered via numpy's randn command, which is not truly random, so the data
-# will be the same through each simulation. Data is segregated by quadrant, X,Y can take
-# four different quadrants. As the data is collected via the randn function, which returns
-# values with mean 0 and standard deviation 1, all points lie inside the range -3, 3, which
-# is purely coincidental since this is not a true random function and is seeded with the
-# same number each time (0). Scikit-learn then makes use of the SVC function, which
-# organizes this data into two distinct groups. Finally, matplotlib.pyplot plots the 
-# colored output data.
+# This project provides the same capabilities as the LogisticRegression.py program
+# but uses a radial basis function kernel for the learning algorithm rather than
+# a sigma function. This allows it to group data into sets with non-linear
+# boundaries.
 
 def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
     # setup marker generator and color map
@@ -49,19 +44,26 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
                 alpha=1.0, linewidth=1, marker='o', \
                 s=55, label='test set')
 
-np.random.seed(0)
-X_xor = np.random.randn(200, 2)
-y_xor = np.logical_xor(X_xor[:, 0] > 0, X_xor[:, 1] > 0)
-y_xor = np.where(y_xor, 1, -1)
-g = float(input("Input gamma: "))
-# The gamma value here is effectively a cutoff parameter for a normal distribution sphere
-# which is used to vary the decision boundary between groups.
-# See the help page for more details: help(sklearn.svm.SVC)
-svm = SVC(kernel='rbf', C=10.0, gamma=g, random_state = 0)
-svm.fit(X_xor, y_xor)
+iris = datasets.load_iris()
+X = iris.data[:, [2,3]]
+y = iris.target
 
-plot_decision_regions(X_xor, y_xor, classifier=svm, test_idx=range(105, 150))
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+
+g = float(input("Input gamma: "))
+
+svm = SVC(C=1.0, kernel='rbf',gamma=g, random_state = 0, probability=True)
+svm.fit(X_train_std, y_train)
+
+plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105, 150))
 plt.show()
 
-#for X_test_val in X_test_std:
-#    print(lr.predict_proba(X_test_val.reshape(1, -1)))
+for X_test_val in X_test_std:
+    print(svm.predict_proba(X_test_val.reshape(1, -1)))
