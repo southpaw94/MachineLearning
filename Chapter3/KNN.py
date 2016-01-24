1504e3044da8d8f19c4e30b76b94cec40a24fa6e
Michael Estes <mjestes1994@gmail.com>
2016-01-24 17:44:28 -0500

    Added chapter 4 examples in DataPreprocessing directory. These examples show how to segregate data by most important features, and how to manually find most important features via L1 regularization.

---
+++ Chapter3/KNN.py
@@ -0,0 +1,33 @@
+from sklearn import datasets
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+
+from matplotlib.colors import ListedColormap
+
+import numpy as np
+import matplotlib.pyplot as plt\
+
+from plots import PlotFigures
+
+iris = datasets.load_iris()
+X = iris.data[:, [2,3]]
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+
+knn = KNeighborsClassifier(n_neighbors=5, p=2, metric='minkowski')
+knn.fit(X_train_std, y_train)
+
+PlotFigures.plot_decision_regions(X_combined_std, y_combined, classifier=knn, test_idx=range(105, 150))
+plt.show()
+
+#for X_test_val in X_test_std:
+#    print(lr.predict_proba(X_test_val.reshape(1, -1)))
