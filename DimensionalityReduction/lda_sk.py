7fad90413dbb0b7027e9493c9aa74fda954a9836
Michael Estes <mjestes1994@gmail.com>
2016-01-31 13:19:08 -0500

    Created two linear discriminant analysis files.

--- DimensionalityReduction/lda_sk.py
+++ DimensionalityReduction/lda_sk.py
@@ -2,10 +2,14 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.decomposition import PCA
 from sklearn.cross_validation import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from plots import plot_decision_regions
-import matplotlib.pyplot as plt 
 
+import matplotlib.pyplot as plt 
+import numpy as np
 import pandas as pd
+
+np.set_printoptions(precision=4)
 
 pca = PCA(n_components = 2)
 lr = LogisticRegression()
@@ -17,24 +21,25 @@ y = data.iloc[:, 0].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 scaler = StandardScaler()
 X_train_std = scaler.fit_transform(X_train)
-X_test_std = scaler.fit_transform(X_test)
+X_test_std = scaler.transform(X_test)
 
-X_train_pca = pca.fit_transform(X_train_std)
-X_test_pca = pca.transform(X_test_std)
+lda = LDA(n_components = 2)
+X_train_lda = lda.fit_transform(X_train_std, y_train)
 
-lr.fit(X_train_pca, y_train)
-plot_decision_regions(X_train_pca, y_train, classifier = lr)
-plt.xlabel('PC1')
-plt.ylabel('PC2')
+lr = LogisticRegression()
+lr = lr.fit(X_train_lda, y_train)
+
+plot_decision_regions(X_train_lda, y_train, classifier=lr)
+plt.xlabel('LD 1')
+plt.ylabel('LD 2')
 plt.legend(loc='lower left')
+plt.title('Training Data')
 plt.show()
 
-plot_decision_regions(X_test_pca, y_test, classifier=lr)
-plt.xlabel('PC1')
-plt.ylabel('PC2')
+X_test_lda = lda.transform(X_test_std)
+plot_decision_regions(X_test_lda, y_test, classifier=lr)
+plt.xlabel('LD 1')
+plt.ylabel('LD 2')
 plt.legend(loc='lower left')
+plt.title('Test Data')
 plt.show()
-
-pca = PCA(n_components = None)
-X_train_pca = pca.fit_transform(X_train_std)
-print(pca.explained_variance_ratio_)
