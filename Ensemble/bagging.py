9b53b4ec111b2a3d62be0a3857e604c56a29b5af
Michael J Estes <mjestes1994@gmail.com>
2016-02-14 22:53:59 -0500

    Finished ensemble learning chapter. Implemented AdaBoost algorithm to iris_sklearn.py in Ensemble directory.

--- Ensemble/bagging.py
+++ Ensemble/bagging.py
@@ -4,6 +4,10 @@ from sklearn.cross_validation import train_test_split
 from sklearn.ensemble import BaggingClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score  
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import AdaBoostClassifier
+import numpy as np
+import matplotlib.pyplot as plt
 
 df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', header=None)
 df_wine.columns = ['Class label', 'Alcohol', 
@@ -19,6 +23,9 @@ df_wine.columns = ['Class label', 'Alcohol',
 df_wine = df_wine[df_wine['Class label'] != 1]
 y = df_wine['Class label'].values
 X = df_wine[['Alcohol', 'Hue']].values
+
+le = LabelEncoder()
+y = le.fit_transform(y)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 tree = DecisionTreeClassifier(criterion='entropy', 
@@ -46,3 +53,47 @@ bag_train = accuracy_score(y_train, y_train_pred)
 bag_test = accuracy_score(y_test, y_test_pred)
 print('Bagging train/test accuracies: %.3f/%.3f'
         % (bag_train, bag_test))
+
+ada = AdaBoostClassifier(base_estimator=tree,
+        n_estimators=500,
+        learning_rate=0.1)
+ada = ada.fit(X_train, y_train)
+y_train_pred = ada.predict(X_train)
+y_test_pred = ada.predict(X_test)
+ada_train = accuracy_score(y_train, y_train_pred)
+ada_test = accuracy_score(y_test, y_test_pred)
+print('AdaBoost train/test accuracies %.3f/%.3f' 
+        % (ada_train, ada_test))
+
+x_min = X_train[:, 0].min() - 1
+x_max = X_train[:, 0].max() + 1
+y_min = X_train[:, 1].min() - 1
+y_max = X_train[:, 1].max() + 1
+
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
+        np.arange(y_min, y_max, 0.1))
+
+f, axarr = plt.subplots(nrows=1, ncols=3,
+        sharex='col',
+        sharey='row',
+        figsize=(8, 3))
+
+for idx, clf, tt in zip([0, 1, 2], [tree, bag, ada], ['Decision Tree', 'Bagging', 'AdaBoost']):
+    clf.fit(X_train, y_train)
+
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    axarr[idx].contourf(xx, yy, Z, alpha=0.3)
+    axarr[idx].scatter(X_train[y_train==0, 0],
+            X_train[y_train==0, 1],
+            c='blue', marker='^')
+    axarr[idx].scatter(X_train[y_train==1, 0],
+            X_train[y_train==1, 1],
+            c='red', marker='o')
+    axarr[idx].set_title(tt)
+
+axarr[0].set_ylabel('Alcohol', fontsize=12)
+plt.text(10.2, -1.2, s='Hue', ha='center', 
+        va='center', fontsize=12)
+plt.show()
